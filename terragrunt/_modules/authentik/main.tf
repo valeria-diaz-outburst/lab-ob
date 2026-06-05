5be6785702f116353f07ee2f9d50635c7b97a278
valeria-diaz-outburst <253562810+valeria-diaz-outburst@users.noreply.github.com>
2026-06-05 12:44:47 -0600

    Authentik implemenation

--- terragrunt/_modules/authentik/main.tf
+++ terragrunt/_modules/authentik/main.tf
@@ -14,7 +14,8 @@ resource "helm_release" "authentik" {
   repository = "https://charts.goauthentik.io"
   chart      = "authentik"
   version    = var.chart_version
-  namespace  = kubernetes_namespace.deployment.metadata[0].name
+  namespace  = kubernetes_namespace.authentik.metadata[0].name
+  wait       = false
 
   values = [<<EOF
 authentik:
@@ -26,9 +27,13 @@ postgresql:
   enabled: true
   auth:
     password: "${data.vault_kv_secret_v2.authentik.data["postgresql_password"]}"
+  image:
+    tag: "17"
 
 redis:
   enabled: true
+  image:
+    tag: "7"
 EOF
   ]
 

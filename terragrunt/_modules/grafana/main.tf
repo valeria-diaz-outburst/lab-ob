0673fa43b5e6287f1791d4d5ad3d819f6c96e5ab
valeria-diaz-outburst <253562810+valeria-diaz-outburst@users.noreply.github.com>
2026-06-18 16:53:49 -0600

    Deploy Grafana

--- terragrunt/_modules/grafana/main.tf
+++ terragrunt/_modules/grafana/main.tf
@@ -57,8 +57,42 @@ resource "kubernetes_service" "grafana" {
       target_port = 3000
     }
 
-    type = "LoadBalancer"
+    type = "ClusterIP"
   }
 
   depends_on = [kubernetes_deployment.grafana]
+}
+
+resource "kubernetes_ingress_v1" "grafana" {
+  metadata {
+    name      = "grafana"
+    namespace = kubernetes_namespace.grafana.metadata[0].name
+    annotations = {
+      "kubernetes.io/ingress.class" = "traefik"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+
+    rule {
+      host = "grafana.lab"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.grafana.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [kubernetes_service.grafana]
 }
